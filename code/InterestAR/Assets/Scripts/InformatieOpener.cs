b747f55989d14f624f1553eae8f2c7d1f6102725
Tim Maas Geesteranus <tim__mg@hotmail.com>
2019-11-26 10:53:33 +0100

    marker with click function

---
+++ code/InterestAR/Assets/Scripts/InformatieOpener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformatieOpener : MonoBehaviour
+{
+
+    public GameObject Informatie;
+
+    public void openInformatie()
+    {
+        Debug.Log("HALLO");
+        if(Informatie != null)
+        {
+            Informatie.SetActive(!Informatie.activeSelf);
+        }
+    }
+}
---
+++ code/InterestAR/Assets/Scripts/ModelInformatieOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelInformatieOpener : MonoBehaviour
+{
+    public GameObject Informatie;
+
+    public void OnMouseDown()
+    {
+        Debug.Log("HOI");
+        if (Informatie != null)
+        {
+            Informatie.SetActive(!Informatie.activeSelf);
+        }
+    }
+}
