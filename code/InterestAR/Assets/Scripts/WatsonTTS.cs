20e3373924bc31b64bc9bfaab4b1448bd2811461
kay <kayverhaegh@home.nl>
2019-11-26 13:36:45 +0100

    Watson intgrated

---
+++ code/InterestAR/Assets/Scripts/WatsonTTS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IBM.Cloud.SDK;
+using IBM.Cloud.SDK.Utilities;
+using IBM.Watson.TextToSpeech.V1;
+
+public class WatsonTTS : MonoBehaviour
+{
+    TextToSpeechService textToSpeechService;
+
+
+    public void Wrapper(){
+        StartCoroutine(Speak());
+    }
+
+    public IEnumerator Speak()
+    {
+        textToSpeechService = new TextToSpeechService();
+        while (!textToSpeechService.Authenticator.CanAuthenticate())
+            yield return null;
+
+        byte[] synthesizeResponse = null;
+        AudioClip clip = null;
+        textToSpeechService.Synthesize(
+            callback: (DetailedResponse<byte[]> response, IBMError error) =>
+            {
+                synthesizeResponse = response.Result;
+                clip = WaveFile.ParseWAV("hello_world.wav", synthesizeResponse);
+                AudioSource audioSource = GetComponent<AudioSource>();
+                audioSource.clip = clip;
+                audioSource.Play();
+            },
+            text: "Hello world",
+            voice: "en-US_AllisonVoice",
+            accept: "audio/wav"
+        );
+
+        while (synthesizeResponse == null)
+        {
+            yield return null;
+        }
+    }
+}
