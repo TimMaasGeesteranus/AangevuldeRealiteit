30054f298fd6ceb5db06cbaa5313a115393d831b
Kevin van Schaijk <kevin.vanschaijk@home.nl>
2019-12-02 21:04:01 +0100

    settings UI

---
+++ code/InterestAR/Assets/Scripts/settings/UserData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData
+{
+    // In app language
+    public string Language { get; set; }
+
+    // Range in KM
+    public string Range { get; set; }
+}
