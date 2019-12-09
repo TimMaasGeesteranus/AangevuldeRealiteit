324fac150e17a9261c1febf3912fe739dcb122ff
Kevin van Schaijk <kevin.vanschaijk@home.nl>
2019-12-09 15:24:37 +0100

    settings fixed

---
+++ code/InterestAR/Assets/Scripts/Services/SaveSystem.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.settings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.SaveSystem
+{
+    static class SaveSystem
+    {
+        private static string PerisistencePath = Path.Combine(Application.persistentDataPath, "settings.json");
+
+        // Saves the userdata to the persistent data directory
+        public static void saveSettings(UserData userData) {
+            File.WriteAllText(PerisistencePath, JsonUtility.ToJson(userData));
+        }
+
+        // Gets the userdata
+        public static UserData loadSettings()
+        {
+            StreamReader reader = new StreamReader(PerisistencePath);
+            string json = reader.ReadToEnd();
+            return JsonUtility.FromJson<UserData>(json);
+        }
+    }
+}
