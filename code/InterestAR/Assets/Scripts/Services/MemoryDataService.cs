dedf7254271f35d742338bf0ae67d08c17722110
RandyGrouls <randy_grouls@live.nl>
2019-12-10 11:39:15 +0100

    revert commit

---
+++ code/InterestAR/Assets/Scripts/Services/MemoryDataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryDataService
+{
+    private const string LANGUAGE = "language";
+    private const string DISTANCE = "distance";
+
+    public static string Language
+    {
+        get { return PlayerPrefs.GetString(LANGUAGE, "English"); }
+        set { PlayerPrefs.SetString(LANGUAGE, value); }
+    }
+    public static float Distance
+    {
+        get { return PlayerPrefs.GetFloat(DISTANCE, 50); }
+        set { PlayerPrefs.SetFloat(DISTANCE, value); }
+    }
+
+    public static void DirectSave()
+    {
+        PlayerPrefs.Save();
+    }
+}
---
+++ code/InterestAR/Assets/Scripts/Services/SaveSystem.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.settings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.SaveSystem
+{
+    static class SaveSystem
+    {
+        private static string PersistencePath = Path.Combine(Application.persistentDataPath, "settings.json");
+
+        // Saves the userdata to the persistent data directory
+        public static void saveSettings(UserData userData)
+        {
+            File.WriteAllText(PersistencePath, JsonUtility.ToJson(userData));
+        }
+
+        // Gets the userdata
+        public static UserData loadSettings()
+        {
+            StreamReader reader = new StreamReader(PersistencePath);
+            string json = reader.ReadToEnd();
+            return JsonUtility.FromJson<UserData>(json);
+        }
+    }
+}
