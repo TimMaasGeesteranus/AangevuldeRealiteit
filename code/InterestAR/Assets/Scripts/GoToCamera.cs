96fcc75ec5dca1e25edad4f3ac88e4d43314dd77
Jim van de Ligt <jimvandeligt@hotmail.com>
2019-12-11 12:07:54 +0100

    Updated button + Added close button to Settings. Saving returns you to Camera

---
+++ code/InterestAR/Assets/Scripts/GoToCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToCamera : MonoBehaviour
+{
+    public Button CloseButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = CloseButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("CameraScene");
+    }
+
+    void Update()
+    {
+    }
+}
