2bf94c329e41e3f1a545a76791dc4e46d05281d5
Kev4ever <kevin.van.schaijk01@gmail.com>
2019-12-18 12:24:14 +0100

    GPS added
    Not working yet (displays long - lat 0)

---
+++ code/InterestAR/Assets/Scripts/GPS.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+    public float Latitude;
+    public float Longitude;
+
+    public static GPS Instance;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        Latitude = Input.location.lastData.latitude;
+        Longitude = Input.location.lastData.longitude;
+    }
+
+
+}
---
+++ code/InterestAR/Assets/Scripts/UpdateGPSText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour
+{
+    public Text Coordinates;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Coordinates.text = "LAT:" + GPS.Instance.Latitude.ToString() + "LON" + GPS.Instance.Longitude.ToString();
+    }
+}
