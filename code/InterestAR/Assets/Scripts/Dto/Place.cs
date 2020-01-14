b040417d7d3e8cc30b804c1a713d27fae7adca7f
Tim Maas Geesteranus <tim__mg@hotmail.com>
2020-01-14 15:34:48 +0100

    Revert "Revert "service for google maps data made""
    This reverts commit 76c82f69b5ab4f5be71e74cfb86e3fc56db7ee08.

---
+++ code/InterestAR/Assets/Scripts/Dto/Place.cs
@@ -0,0 +1,7 @@
+﻿public class Place
+{
+
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+    public string Name { get; set; }
+}
---
+++ code/InterestAR/Assets/Scripts/Services/GoogleMapsService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleMapsService
+{
+
+    public List<Place> GetCoördinates(string latitude, string longitude, int radius)
+    {
+        string googleMapsURL = $"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&type=point_of_interest&key=AIzaSyDWnKL07Y0zk_IHGypBUiUF2Tz_INMeu6c";
+        //string googleMapsURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.825764,%205.865534&radius=1000&type=point_of_interest&key=AIzaSyDWnKL07Y0zk_IHGypBUiUF2Tz_INMeu6c";
+        var json = new WebClient().DownloadString(googleMapsURL);
+        var details = JObject.Parse(json);
+
+        var locations = details["results"];
+
+        List<Place> places = new List<Place>();
+        foreach(var location in locations)
+        {
+     
+            double.TryParse(location["geometry"]["location"]["lat"].ToString(), out double lat);
+            double.TryParse(location["geometry"]["location"]["lng"].ToString(), out double lng);
+            double.TryParse(location["name"].ToString(), out double ba);
+
+           places.Add(new Place()
+            {
+                Lat = lat,
+                Lng = lng,
+                Name = location["name"].ToString()
+            });
+        }
+
+        return places;
+     }
+}
