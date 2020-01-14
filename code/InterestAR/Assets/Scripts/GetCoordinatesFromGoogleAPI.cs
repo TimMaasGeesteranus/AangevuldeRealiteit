3e5c33fa47b3c0b2b0d306bceeb664580cac955f
Tim Maas Geesteranus <tim__mg@hotmail.com>
2020-01-14 13:44:59 +0100

    display info on markerscreen

--- code/InterestAR/Assets/Scripts/GetCoordinatesFromGoogleAPI.cs
+++ code/InterestAR/Assets/Scripts/GetCoordinatesFromGoogleAPI.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 
 public class GetCoordinatesFromGoogleAPI : MonoBehaviour
 {
-    public string PointOfInterest;
     public Text ChangingText;
     
     void Start()
@@ -17,13 +16,26 @@ public class GetCoordinatesFromGoogleAPI : MonoBehaviour
         var json = new WebClient().DownloadString("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.825764,%205.865534&radius=1000&type=point_of_interest&key=AIzaSyD1akMvc1-cVQ_KaZBYgCgzEOdlmAopN10");
         var details = JObject.Parse(json);
         var results = details["results"];
+
+        var opgehaaldeData = new List<string>();
+
         foreach (var obj in results)
         {
             // Debug.Log(string.Concat("Name: ", (obj["name"]), "  -  Lat: ", ((obj["geometry"])["location"])["lat"], " - Let: ", ((obj["geometry"])["location"])["lng"]));
 
-            string latitude = (((obj["geometry"])["location"])["lat"]).ToString();
-            Debug.Log(latitude);
-            double latitudeDouble = Convert.ToDouble(latitude);
+            var lat = (((obj["geometry"])["location"])["lat"]).ToString();
+            var lng = (((obj["geometry"])["location"])["lng"]).ToString();
+            var name = (obj["name"]).ToString();
+
+            string[] input = { lat, lng, name };
+            opgehaaldeData.AddRange(input);
         };
+
+        ChangingText.text = opgehaaldeData.ToString();
+        string weergaveTekst = "";
+        foreach(object o in opgehaaldeData){
+            weergaveTekst = string.Concat(weergaveTekst, o, " - ");
+        }
+        ChangingText.text = weergaveTekst;
     }
 }
