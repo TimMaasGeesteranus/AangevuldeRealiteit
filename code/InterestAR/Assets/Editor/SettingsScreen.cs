2174f9837d9ab9fd2f0b9629d620b744538a06eb
Kev4ever <kevin.van.schaijk01@gmail.com>
2020-01-14 10:32:10 +0100

    tests

---
+++ code/InterestAR/Assets/Editor/SettingsScreen.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class SettingsScreen
+    {
+        private SettingsMenu settingsMenu;
+        private Component[] components;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            settingsMenu = new SettingsMenu();
+            Scene scene = SceneManager.GetSceneByName("SettingsScreen");
+
+            var ui = SearchGameObject("UI",scene.GetRootGameObjects());
+            components = ui.GetComponentsInChildren(GetType());
+
+            settingsMenu.saveButton = (Button)SearchComponents("SaveButton", components);
+            settingsMenu.distanceAmount = (Text)SearchComponents("Label (Distance)", components);
+            settingsMenu.languageDropdown = (Dropdown)SearchComponents("Dropdown", components);
+            settingsMenu.radiusSlider = (Slider)SearchComponents("Slider", components);
+            settingsMenu.radiusImage = (SVGImage) SearchComponents("RadiusImage", components);
+        }
+
+        private static GameObject SearchGameObject(string name, GameObject[] objects)
+        {
+            GameObject selected = null;
+            foreach (var gameobject in objects)
+            {
+                if (gameobject.name == name)
+                {
+                    selected = gameobject;
+                    break;
+                }
+            }
+            return selected;
+        }
+
+        private static Component SearchComponents(string name, Component[] objects)
+        {
+            Component selected = null;
+            foreach (var component in objects)
+            {
+
+                if (component.name == name)
+                {
+                    selected = component;
+                    break;
+                }
+            }
+            return selected;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(settingsMenu);
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SettingsScreenTestSimplePasses()
+        {
+           
+        }
+
+
+        [Test]
+        public IEnumerator SettingsScreenTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ code/InterestAR/Assets/Tests/SettingsScreenTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SettingsScreenTest
+    {
+        private SettingsMenu settingsMenu;
+        GameObject[] gameObjects;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            Scene s = SceneManager.GetSceneByName("SettingsScreen");
+            GameObject[] gameObjects = s.GetRootGameObjects();
+
+            Debug.Log(gameObjects.Length);
+
+            settingsMenu = new SettingsMenu();
+            //settingsMenu.saveButton = new Button();
+
+            //GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+            //List<GameObject> objList = GameObjectEventsHandler.specificSceneObjects["your scene you want"];
+
+            //MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(settingsMenu);
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SettingsScreenTestSimplePasses()
+        {
+            Assert.AreEqual(-1, gameObjects.Length);
+        }
+
+
+        [Test]
+        public IEnumerator SettingsScreenTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
